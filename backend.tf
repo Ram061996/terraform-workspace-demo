8e0956dc7f52a086bbef188a5f934b13454da67a
Rameshwar Korde <101036700+Ram061996@users.noreply.github.com>
2022-04-14 16:57:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ctl-terraform-state-ap-south-1"
-    key            = "tfdemo_workspaces"
+    key            = "tfdemo_workspace"
     region         = "ap-south-1"
     dynamodb_table = "terraform-state"
   }
