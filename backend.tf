8551b2be441c52464fef980375245407828f6d3e
Rameshwar Korde <101036700+Ram061996@users.noreply.github.com>
2022-04-14 14:50:46 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "ctl-terraform-state-ap-south-1"
+    key            = "tfdemo"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state"
+  }
+}
